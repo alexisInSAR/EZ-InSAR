01e13b1efa21c5b883359a57046cfdae39a1dffc
Alexis Hrysiewicz <107697895+alexisInSAR@users.noreply.github.com>
2022-07-06 10:11:18 +0100

    Version 1.0.0 Beta

--- MIESAR_BIN/addpath_MIESAR.m
+++ MIESAR_BIN/addpath_MIESAR.m
@@ -1,11 +1,11 @@
-%%%%%%% Script to set the paths for MIESAR %%%%%%%%%%%%%%
+%%%%%%% Script to set the paths for EZ-InSAR %%%%%%%%%%%%%%
 %% in your startup.m file you should have:
 %%
 % disp('Setting paths for MIESAR: Matlab Interface for Easy InSAR...')
 % run( [ getenv('MIESAR_HOME') filesep 'addpath_MIESAR'] )
 %%
 
-disp('Added to path: MIESAR-v1.0')
+disp('Added to path: EZ-InSAR Version 1.0.0 Beta')
 libdir = [ getenv('MIESAR_BIN') filesep '.'  ];  
 addpath(genpath(libdir),'-end'); 
 
