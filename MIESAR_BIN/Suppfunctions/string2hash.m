70c9d84df1d0142f23bc8eb6ec905b48166b0a58
Alexis Hrysiewicz <107697895+alexisInSAR@users.noreply.github.com>
2022-07-06 13:28:33 +0100

    Version 1.0.0 Beta

--- MIESAR_BIN/Suppfunctions/string2hash.m
+++ MIESAR_BIN/Suppfunctions/string2hash.m
@@ -1,4 +1,19 @@
 function hash = string2hash(string)
+%   string2hash(string)
+%
+%       Function to generate an unique user id. 
+%          
+%       Script from EZ-InSAR toolbox: https://github.com/alexisInSAR/EZ-InSAR
+%
+%   -------------------------------------------------------
+%   Alexis Hrysiewicz, UCD / iCRAG
+%   Version: 1.0.0 Beta
+%   Date: 06/07/2022
+%
+%   -------------------------------------------------------
+%   Version history:
+%           1.0.0 Beta: Initial (unreleased)
+
 persistent md
 if isempty(md)
     md = java.security.MessageDigest.getInstance('MD5');
