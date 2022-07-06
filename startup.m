492e38a78234b42d16a396aebd6ce0e546ba4a55
Alexis Hrysiewicz <107697895+alexisInSAR@users.noreply.github.com>
2022-07-06 09:58:46 +0100

    Version 1.0.0 Beta

--- startup.m
+++ startup.m
@@ -1,3 +1,3 @@
 %% Setting for EZ-InSAR
-disp('Setting paths for EZ-InSAR: Matlab Interface for Easy InSAR...')
+disp('Setting paths for EZ-InSAR: Matlab Interface for Easy InSAR (Version 1.0.0 Beta)...')
 run( [ getenv('MIESAR_BIN') filesep 'addpath_MIESAR'] )
