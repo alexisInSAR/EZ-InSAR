55dce9ce386a2171ac7e309801407b6e7d34fa33
Alexis Hrysiewicz <107697895+alexisInSAR@users.noreply.github.com>
2022-07-06 10:24:06 +0100

    Version 1.0.0 Beta

--- MIESAR_BIN/EZ_InSAR.m
+++ MIESAR_BIN/EZ_InSAR.m
@@ -1,6 +1,19 @@
 function EZ_InSAR 
-%   Copyright 2021 Alexis Hrysiewicz, UCD / iCRAG
+%   EZ_InSAR
+%
+%       Script to run EZ-InSAR
+%   
+%       Script from EZ-InSAR toolbox: https://github.com/alexisInSAR/EZ-InSAR
+%
+%   See also MIESAR
+%
+%   -------------------------------------------------------
+%   Alexis Hrysiewicz, UCD / iCRAG
 %   Version: 1.0.0 Beta
 %   Date: 17/06/2022
-MIESAR; 
+%
+%   -------------------------------------------------------
+%   Version history:
+%           1.0.0 Beta: Initial (unreleased)
 
+MIESAR; 
