23c1281549e7dcb9086cef4e57fa05425ca2ac53
Xiaowen Wang <insarwxw@gmail.com>
2022-08-09 17:58:03 +0100

    Update EZ_InSAR.m

--- EZINSAR_BIN/EZ_InSAR.m
+++ EZINSAR_BIN/EZ_InSAR.m
@@ -14,7 +14,7 @@ function EZ_InSAR(src,evt,action,miesar_para)
 %
 %   -------------------------------------------------------
 %   Alexis Hrysiewicz, UCD / iCRAG
-%   Version: 1.0.0 Beta
+%   Version: 2.0.0 Beta
 %   Date: 29/11/2021 
 %
 %   -------------------------------------------------------
